ee81a36f598a9a2ae265fd648bfaab447776f96e
Pillepop <pschwarze@web.de>
2015-02-18 02:58:44 +0100

    update3

--- src/test/java/com/perfectomobile/StcTest.java
+++ src/test/java/com/perfectomobile/StcTest.java
@@ -1,8 +1,13 @@
 package com.perfectomobile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
@@ -13,11 +18,15 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
+import com.perfectomobile.httpclient.MediaType;
+import com.perfectomobile.httpclient.utils.FileUtils;
 import com.perfectomobile.selenium.MobileDriver;
 import com.perfectomobile.selenium.MobileDevice;
 import com.perfectomobile.selenium.MobileDriver;
 import com.perfectomobile.selenium.api.IMobileDevice;
+import com.perfectomobile.selenium.api.IMobileDriver;
 import com.perfectomobile.selenium.api.IMobileWebDriver;
+import com.perfectomobile.selenium.options.MobileApplicationInstallOptions;
 import com.perfectomobile.selenium.options.MobileBrowserType;
 import com.perfectomobile.selenium.options.MobileDeviceHomeOptions;
 import com.perfectomobile.selenium.options.MobileDeviceOpenOptions;
@@ -25,11 +34,24 @@ import com.perfectomobile.selenium.by.*;
 
 
 public class StcTest {
-  @Test
-  public void f() {
-	  System.out.println("test run");
-	  testrun();
+	private  MobileDriver driver;
+	private IMobileDevice device;
+  
+	@Test(dataProvider = "dp")
+	public void f(Integer n, String s) {
+	  System.out.println("test run "+s);
+	  //uploadLatestBuild();
+	  //install(s);
+	  testrun(s);
 	  }
+	
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "1E674EB8" },
+      new Object[] { 2, "219595A5" },
+    };
+  }
   @BeforeMethod
   public void beforeMethod() {
   }
@@ -48,10 +70,15 @@ public class StcTest {
 
   @BeforeTest
   public void beforeTest() {
+	  driver  = new MobileDriver("demo.perfectomobile.com", "philipps@perfectomobile.com", "Perfect0123");
   }
 
   @AfterTest
   public void afterTest() {
+	  device.close();
+	  System.out.println("finished");
+	  driver.quit();
+	  downloadReport(driver, "results.html");
   }
 
   @BeforeSuite
@@ -62,11 +89,37 @@ public class StcTest {
   public void afterSuite() {
   }
 
-  public void testrun(){
+  public void uploadLatestBuild(){
+	  try {
+		 driver.uploadMedia("PUBLIC:Philipp\\builds\\STC.apk","C:\\mybuild\\MySTC_com.stc_2.4.0_46.apk");
+	} catch (Exception e) {
+		// TODO: handle exception
+		System.out.println(e.getMessage());
+	}
+  }
+
+public void install(String deviceID){
+	  device = driver.getDevice(deviceID);
+	  
+	  try {
+		  device.installApplication("PUBLIC:Philipp\\builds\\STC.apk");
+		  MobileApplicationInstallOptions installOptions9 = new MobileApplicationInstallOptions();
+		  installOptions9.setInstrument(false);
+		  device.installApplication("PUBLIC:Philipp\\builds\\STC.apk", installOptions9);
+
+
+		
+	} catch (Exception e) {
+		System.out.println(e.getMessage());
+		// TODO: handle exception
+	}
+}
+  
+  public void testrun(String deviceID){
 	  //MobileDriver driver = new MobileDriver();
-	  MobileDriver driver = new MobileDriver("demo.perfectomobile.com", "philipps@perfectomobile.com", "Perfect0123");
+	 
 	 // IMobileDevice device = driver.getDevice("219595A5"); //S4
-	  IMobileDevice device = driver.getDevice("1E674EB8"); //S5
+	  device = driver.getDevice(deviceID); //S5
 	  MobileDeviceOpenOptions openOptions2 = new MobileDeviceOpenOptions();
 	  device.open(openOptions2);
 	  MobileDeviceHomeOptions homeOptions1 = new MobileDeviceHomeOptions();
@@ -76,15 +129,17 @@ public class StcTest {
       IMobileWebDriver webDriver = 	device.getDOMDriver(MobileBrowserType.DEFAULT);
       IMobileWebDriver nativeDriver = device.getNativeDriver();
       	  
-
-	  sleep(3000);
-	  visualDriver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-	  visualDriver.findElement(By.linkText("Email Kamera"));
-	  
-	  device.close();
-	  System.out.println("finished");
-	  
-
+      device.getNativeDriver("MySTC").open();
+      visualDriver.findElementByLinkText("MySTC Account");
+		visualDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		visualDriver.manageMobile().visualOptions().validationOptions().setThreshold(90);
+		
+		try{
+		visualDriver.findElement(By.linkText("MyStc account"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}		
   }
 	private static void sleep(long millis) {
 		try {
@@ -92,5 +147,18 @@ public class StcTest {
 		} catch (InterruptedException e) {
 		}
 	}
+	 
+	private static void downloadReport(IMobileDriver driver, String reportname) {
+         InputStream reportStream = driver.downloadReport(MediaType.HTML);
+         if (reportStream != null) {
+                         File reportFile = new File("C:\\Results\\"+reportname);
+                         try {
+							FileUtils.write(reportStream, reportFile);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+ }
+}
   
 }
