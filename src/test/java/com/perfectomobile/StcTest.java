96eb115b0cf940db7a531081b20ffa3c0d772268
Pillepop <pschwarze@web.de>
2015-02-18 23:37:39 +0100

    daimler

--- src/test/java/com/perfectomobile/StcTest.java
+++ src/test/java/com/perfectomobile/StcTest.java
@@ -20,9 +20,11 @@ import org.testng.annotations.AfterSuite;
 
 import com.perfectomobile.httpclient.MediaType;
 import com.perfectomobile.httpclient.utils.FileUtils;
+import com.perfectomobile.selenium.MobileAddressLocation;
 import com.perfectomobile.selenium.MobileDriver;
 import com.perfectomobile.selenium.MobileDevice;
 import com.perfectomobile.selenium.MobileDriver;
+import com.perfectomobile.selenium.MobileLocation;
 import com.perfectomobile.selenium.api.IMobileDevice;
 import com.perfectomobile.selenium.api.IMobileDriver;
 import com.perfectomobile.selenium.api.IMobileWebDriver;
@@ -36,27 +38,33 @@ import com.perfectomobile.selenium.by.*;
 public class StcTest {
 	private  MobileDriver driver;
 	private IMobileDevice device;
-	private String repositoryPath = "PUBLIC:Philipp\\builds\\STC.apk";
-	private String localPath = "C:\\mybuild\\MySTC_com.stc_2.4.0_46.apk";
-	private String appIdentifier = "MySTC";
-  
+	//private String repositoryPath = "PUBLIC:Philipp\\builds\\STC.apk";
+	//private String localPath = "C:\\mybuild\\MySTC_com.stc_2.4.0_46.apk";
+	//private String appIdentifier = "MySTC";
+	private String repositoryPath = "PUBLIC:Philipp\\builds\\car2go_com.car2go_2.2.3_20203.apk";
+	private String localPath = "C:\\mybuild\\car2go_com.car2go_2.2.3_20203.apk";
+	private String appIdentifier = "car2go";
+	
+
 	@Test(dataProvider = "dp")
 	public void f(Integer n, String s) {
+	  
 	  System.out.println("test run "+s);
 	  device = driver.getDevice(s);
 	  device.open();	  
 	 	  
-	  install(s);
-	  testrun(s);
+//	  install(s);
+	  testCar2Go(s);
 	  }
+
 	
-  @DataProvider
-  public Object[][] dp() {
-    return new Object[][] {
-      new Object[] { 1, "1E674EB8" },
-      new Object[] { 2, "219595A5" },
-    };
-  }
+	  @DataProvider
+	  public Object[][] dp() {
+	    return new Object[][] {
+	      new Object[] { 1, "1E674EB8" },
+	      new Object[] { 2, "219595A5" },
+	    };
+	  }
  /* @BeforeMethod
   public void beforeMethod() {
 
@@ -110,6 +118,12 @@ public class StcTest {
 public void install(String deviceID){
  
 	  try {
+		  try {
+			  	device.getNativeDriver("Car2Go").uninstall();
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		  
 		  device.installApplication(repositoryPath);
 //		  MobileApplicationInstallOptions installOptions9 = new MobileApplicationInstallOptions();
 //		  installOptions9.setInstrument(false);
@@ -131,12 +145,49 @@ public void install(String deviceID){
       IMobileWebDriver webDriver = 	device.getDOMDriver(MobileBrowserType.DEFAULT);
       IMobileWebDriver nativeDriver = device.getNativeDriver();
       	  
-      device.getNativeDriver("MySTC").open();		
+      device.getNativeDriver(appIdentifier).open();		
 		
 		try{
 		visualDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		visualDriver.manageMobile().visualOptions().validationOptions().setThreshold(90);	
 		visualDriver.findElement(By.linkText("MyStc account"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			// TODO: handle exception
+		}		
+		device.close();
+  }
+  public void testCar2Go(String deviceID){
+	  //MobileDriver driver = new MobileDriver();
+	  	 
+	  MobileDeviceHomeOptions homeOptions1 = new MobileDeviceHomeOptions();
+	  device.home(homeOptions1);
+
+	  IMobileWebDriver visualDriver = device.getVisualDriver();
+      IMobileWebDriver webDriver = 	device.getDOMDriver(MobileBrowserType.DEFAULT);
+      IMobileWebDriver nativeDriver = device.getNativeDriver();
+      	  
+      device.getNativeDriver(appIdentifier).open();		
+		
+		try{
+			visualDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			visualDriver.manageMobile().visualOptions().validationOptions().setThreshold(95);
+			visualDriver.findElement(By.linkText("Google-Kartendaten 2015 Google, GeoBasis-DE/BKG"));
+
+/*			MobileLocation location1 = new MobileAddressLocation("Münchner Freiheit, München");
+			device.setLocation(location1);
+
+			webDriver.findElement(By.xpath("//*[@contentDesc=\"Position bestimmen\"]")).click();
+			sleep(3000);*/
+			MobileLocation location2 = new MobileAddressLocation("Wilhelm-Runge-Straße 11, Ulm");
+			driver.getDevice("1E674EB8").setLocation(location2);
+
+			webDriver.findElement(By.xpath("//*[@contentDesc=\"Position bestimmen\"]")).click();
+			
+			visualDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			visualDriver.manageMobile().visualOptions().validationOptions().setThreshold(80);
+			visualDriver.findElement(By.linkText("Car2Go"));
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			// TODO: handle exception
