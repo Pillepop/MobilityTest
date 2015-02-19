b4d9b1eb19257fd104122136a07a0aaf9f6b7e04
Pillepop <pschwarze@web.de>
2015-02-19 11:15:50 +0100

    MorningBuild6

---
+++ src/test/java/com/perfectomobile/Constants.java
@@ -0,0 +1,13 @@
+package com.perfectomobile;
+
+public class Constants
+{
+/** Project Constants */
+public static final String PM_PASSWORD = "Perfecto123";
+public static final String PM_USER = "jenkins@perfectomobile.com";
+public static final String PM_CLOUD = "demo.perfectomobile.com";
+
+public static final String REPORT_LIB = "c:\\test\\";
+public static final String REPORT_LIB_HTML = "c:/test/";
+
+}
